057fc7d02a1d12dbe238e464ca5a0ac28f31cff6
Ashish Jha <125789202+jhaashish03@users.noreply.github.com>
2023-04-16 19:51:37 +0530

    Delete graphqlpractice directory

--- graphqlpractice/src/main/java/com/graphqlpractice/dao/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.graphqlpractice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.graphqlpractice.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer>{
-
-}
