cf459a86a49197889d40a203d7ecc0cca10ae3a9
Ashish Jha <125789202+jhaashish03@users.noreply.github.com>
2023-04-16 19:17:53 +0530

    first commit

---
+++ graphqlpractice/src/test/java/com/graphqlpractice/GraphqlpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqlpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
